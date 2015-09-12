ae2874b774fc5629bf19eb674d2376a8f92fe230
Matias <matiasjunemann@gmail.com>
2015-09-12 19:04:20 -0300

    avance 3

--- Tarea1/implementation/vector.c
+++ Tarea1/implementation/vector.c
@@ -39,7 +39,7 @@ void vector_destroy(vector *vector)
 	if(vector->freeFn) {
 		int i;
 		for(i = 0; i < vector_size(vector); i++) {
-			void *target = vector(addres(vector,i));
+			void *target = vector_address(vector,i);
 			if(!*(int *)target)
 			{
 			vector->freeFn(target);
