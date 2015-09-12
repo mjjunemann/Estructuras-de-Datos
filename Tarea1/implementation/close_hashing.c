2a92f9a45d5e0cd69202d3a40eac60db7e780fb3
Matias <matiasjunemann@gmail.com>
2015-09-12 16:05:03 -0300

    testing5

--- Tarea1/implementation/close_hashing.c
+++ Tarea1/implementation/close_hashing.c
@@ -26,6 +26,7 @@ void hash_new(close_hash *hash,int elementSize,freeFunction freeElement,freeFunc
 void hash_destroy(close_hash *hash)
 {
   vector_destroy(hash->array);
+  free(hash->vector);
 }
 
 void hash_add(close_hash *hash,char *key, void *element)
